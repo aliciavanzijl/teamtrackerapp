e2514db8cd6b39531ea972b74d74bc69c9d086a4
Alicia van Zijl <aliciavanzijl@gmail.com>
2019-10-22 23:13:23 +0200

    Add footer and logo files

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render () {
+
+    return (
+      <div id="footerblock">
+        <footer>This React app is a simple demo created by Alicia van Zijl</footer>
+      </div>
+    )
+  }
+}
+
+export default Footer
