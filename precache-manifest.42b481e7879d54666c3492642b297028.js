8ac4e663e0c553f0338e695134968c8f22ce2c34
Alicia van Zijl <aliciavanzijl@gmail.com>
2019-10-22 23:28:41 +0200

    Updates

---
+++ precache-manifest.42b481e7879d54666c3492642b297028.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3012a21f07ac3c5fd5c73de1593e8165",
+    "url": "/teamtrackerapp/index.html"
+  },
+  {
+    "revision": "377961a02579288fff03",
+    "url": "/teamtrackerapp/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "33d855352eabf0d89540",
+    "url": "/teamtrackerapp/static/css/main.dbd64786.chunk.css"
+  },
+  {
+    "revision": "377961a02579288fff03",
+    "url": "/teamtrackerapp/static/js/2.76255311.chunk.js"
+  },
+  {
+    "revision": "33d855352eabf0d89540",
+    "url": "/teamtrackerapp/static/js/main.f8b6ae8d.chunk.js"
+  },
+  {
+    "revision": "8ad5fb197c34d2a8ecce",
+    "url": "/teamtrackerapp/static/js/runtime-main.3fe30ec7.js"
+  },
+  {
+    "revision": "a320f9a2cf60283fda95588b6743d818",
+    "url": "/teamtrackerapp/static/media/logo.a320f9a2.png"
+  }
+]);
